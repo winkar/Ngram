2ada9dabe111b3ac4e245fddc75c4c4f0b691cea
Yiming Chen <dsdshcym@gmail.com>
2015-12-09 19:49:01 +0800

    Split lines into plain texts

--- mapper.py
+++ mapper.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
+#coding=utf-8
 
 import sys
+import re
+import HTMLParser
+
+parser = HTMLParser.HTMLParser()
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        print line
+        origin_line = line
+        line = line.decode('utf-8')
+        line = parser.unescape(line)
+
+        if line.startswith('#') or line.startswith('!'):
+            continue
+
+        sentences = re.split(r"[^0-9a-zA-Z\s]", line)
+        sentences = [x.strip() for x in sentences if len(x.strip().split()) >= 3]
+        print origin_line, sentences
+        print
