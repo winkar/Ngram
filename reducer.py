bd26372456730d9744885ce7762fb806a23c0ed6
Yiming Chen <dsdshcym@gmail.com>
2015-12-09 21:04:52 +0800

    Count bi_word count in the first round too

--- reducer.py
+++ reducer.py
@@ -3,11 +3,16 @@
 import sys
 from collections import defaultdict
 
-count = defaultdict(int)
+tri_count = defaultdict(int)
+bi_count = defaultdict(int)
 
 if __name__ == '__main__':
     for line in sys.stdin:
         tri_word = tuple(line.split())
-        count[tri_word] += 1
-    for key in count:
-        print count[key], key[0], key[1], key[2]
+        bi_word = tuple(tri_word[:2])
+        tri_count[tri_word] += 1
+        bi_count[bi_word] += 1
+    for key in tri_count:
+        tri_word = key
+        bi_word = key[:2]
+        print bi_count[bi_word], tri_count[tri_word], key[0], key[1], key[2]
