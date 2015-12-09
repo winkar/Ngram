8ffbcb936210b94fcd4fd402ae5737788536d82c
Yiming Chen <dsdshcym@gmail.com>
2015-12-09 20:05:21 +0800

    Emit tri_word to reducer

--- mapper.py
+++ mapper.py
@@ -18,5 +18,9 @@ if __name__ == '__main__':
 
         sentences = re.split(r"[^0-9a-zA-Z\s]", line)
         sentences = [x.strip() for x in sentences if len(x.strip().split()) >= 3]
-        print origin_line, sentences
-        print
+
+        for sentence in sentences:
+            sentence = sentence.split()
+            for tri_word in [sentence[i:i + 3] for i in range(len(sentence) - 3)]:
+                tri_word = map(unicode.lower, tri_word)
+                print tri_word[0], tri_word[1], tri_word[2]
